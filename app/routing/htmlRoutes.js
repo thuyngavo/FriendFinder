011d0783540e23099f4e6026a6e074a9bc11cbcd
thuyngavo <lisa.vo49@gmail.com>
2019-04-24 01:30:01 -0500

    attempt to fix routing bug

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,13 +9,13 @@ var path = require("path");
 //======================================================
 
 module.exports = function(app){
+    // home
+    app.get("/",function (req, res) {
+      res.sendFile(path.join(__dirname + "/../public/home.html"));
+    });
 
-  app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/survey.html"));
-  });
-
-  // default home
-  app.get(function (req, res) {
-    res.sendFile(path.join(__dirname + "/../public/home.html"));
-  });
+    //survey
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
+    });
 };
